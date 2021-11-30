40b4a54df18dced3dc1920945016bf8884d343dd
luis marquez <lmarquez@outlook.com.ar>
2021-11-29 22:35:37 -0300

    first commit

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './NavBar.css'
+import logo from '../../logo.svg'
+import Button from '@mui/material/Button';
+
+
+
+export default function NavBar() {
+
+    return(
+        
+            <nav>
+                <div className="container-logo">
+                    <img src={logo} alt="Logo ecommerce"/>
+                </div> 
+                <ul>
+                    <li><Button variant="contained">Home</Button></li>    
+                    <li><Button variant="contained">Products</Button></li>
+                    <li><Button variant="contained">Contact</Button></li>
+                    <li><Button variant="contained">About</Button></li>
+                </ul>   
+            </nav>
+        
+    )
+
+}
+
+
+
+
+
+
+
